d603c5281e410b317a22a298edb68aa4b61f6621
Jan Kucera <jan.kucera@cesnet.cz>
2024-09-22 15:57:11 +0200

    Redirect to FIT / Zborovska based on IP address

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-header("Location: /doma");
-die;
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+	$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
+if ($_SERVER['REMOTE_ADDR'] == '78.102.201.145') {
+	header("Location: /zborovska");
+	exit(0);
+}
+
+header("Location: /fit");
