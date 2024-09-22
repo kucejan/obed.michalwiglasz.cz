e6026ca3eab85f320fd560c5afd6e9f6d496da48
Jan Kucera <jan.kucera@cesnet.cz>
2024-09-22 14:53:23 +0200

    Fix IP address for Zborovska area

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
 	$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 }
 
-if ($_SERVER['REMOTE_ADDR'] == '89.177.97.105') {
+if ($_SERVER['REMOTE_ADDR'] == '78.102.201.145') {
 	header("Location: /zborovska");
 	exit(0);
 }
