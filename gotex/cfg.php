4fc369c5b5ba55753499ab0d9e715ed9ddd725c6
Adam Skrášek <a.skrasek@gmail.com>
2026-01-01 22:07:37 +0100

    Add Hostinec pod schody

--- gotex/cfg.php
+++ gotex/cfg.php
@@ -12,18 +12,19 @@ if (isset($_GET['force'])) {
 }
 
 $sources = [
-	// sorted by walking time from gotex, from closest to farthest
-	new Source(new FreshMenu),
-	new Source(new MenickaCz(9363, 'Bistro na Botance', 'https://www.facebook.com/BistroNaBotance', 'botanka')),
-	new Source(new MenickaCz(5448, 'Light of India', 'http://www.lightofindia.cz/lang-cs/denni-menu', 'india')),
-	new Source(new MenickaCz(2724, 'Plzeňský Dvůr', 'https://www.plzenskydvur.cz/', 'plzen')),
-	new Source(new MenickaCz(3854, 'Na Růžku', 'https://www.naruzkubrno.cz/tydenni-menu/', 'ruzek')),
-	new Source(new MenickaCz(2609, 'Pizzeria Al Capone', 'https://www.pizzaalcapone.cz/poledni-menu', 'alcapone')),
-	new Source(new MenickaCz(6468, 'Divá Bára', 'https://www.restauracedivabara.cz/menu/', 'bara')),
-	new Source(new MenickaCz(2752, 'U Dřeváka Beer&Grill', 'https://udrevaka.cz/pages/poledni-menu', 'drevak')),
+	// sorted by walking time from gotex (old exit to park, 6H6X+PG Brno-Královo Pole for google maps), from closest to farthest 
+	new Source(new FreshMenu), // 0 m
+	new Source(new MenickaCz(9363, 'Bistro na Botance', 'https://www.facebook.com/BistroNaBotance', 'botanka')), // 270m
+	new Source(new MenickaCz(5448, 'Light of India', 'http://www.lightofindia.cz/lang-cs/denni-menu', 'india')), // 350m
+	new Source(new HostinecPodSchody), //400m
+	new Source(new MenickaCz(3854, 'Na Růžku', 'https://www.naruzkubrno.cz/tydenni-menu/', 'ruzek')), //400m
+	new Source(new MenickaCz(2724, 'Plzeňský Dvůr', 'https://www.plzenskydvur.cz/', 'plzen')), //450m
+	new Source(new MenickaCz(2609, 'Pizzeria Al Capone', 'https://www.pizzaalcapone.cz/poledni-menu', 'alcapone')), //500m
+	new Source(new MenickaCz(6468, 'Divá Bára', 'https://www.restauracedivabara.cz/menu/', 'bara')), //500m
+	new Source(new MenickaCz(2752, 'U Dřeváka Beer&Grill', 'https://udrevaka.cz/pages/poledni-menu', 'drevak')), //500m
 	new Source(new Tao),
-	new Source(new MenickaCz(5429, 'Bistro PLAC', 'https://bistroplac.cz/online-menu/section:obedove-menu-11-14h', 'plac')),
-	new Source(new MenickaCz(6694, 'Bistro Ministerio', 'https://ministerio.cz/', 'ministerio')),
-	new Source(new MenickaCz(4116, 'Padagali', 'https://padagali.cz/', 'pagadali')),
-	new Source(new MenickaCz(5416, 'Restaurace Na Halách', 'https://www.sportcentrumluzanky.cz/restaurace/', 'haly')),
+	new Source(new MenickaCz(5429, 'Bistro PLAC', 'https://bistroplac.cz/online-menu/section:obedove-menu-11-14h', 'plac')), //600m
+	new Source(new MenickaCz(6694, 'Bistro Ministerio', 'https://ministerio.cz/', 'ministerio')), //800m
+	new Source(new MenickaCz(4116, 'Padagali', 'https://padagali.cz/', 'pagadali')), //800m
+	new Source(new MenickaCz(5416, 'Restaurace Na Halách', 'https://www.sportcentrumluzanky.cz/restaurace/', 'haly')), //850m
 ];
